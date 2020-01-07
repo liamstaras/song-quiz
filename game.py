dadd050e9c00a4db0052f2257957774868869074
Liam Staras <liam.staras@gmail.com>
2020-01-07 19:29:49 +0000

    Add dummy game routine

--- game.py
+++ game.py
@@ -3,7 +3,29 @@
 
 # import required libraries
 import csv
+import random
+
+# define constants
+ATTEMPTS = 2
 
 # read song database from csv
 with open('songs.csv') as csvfile:
-    songs = list(csv.reader(csvfile))
+    songs = list(csv.reader(csvfile))[1:] # actually read in the songs, skip header row
+
+# game routine
+attempts = 0
+while not(attempts >= ATTEMPTS):
+    attempts = 0
+    song = random.choice(songs)
+    print('Guess the song!')
+    while not(attempts >= ATTEMPTS):
+        if attempts > 0:
+            print('try again')
+        print('Artist: '+song[1])
+        print('Song: '+song[0])
+        guess = input()
+        if guess == song[0]:
+            print('Well done!')
+            break
+        else:
+            attempts += 1
