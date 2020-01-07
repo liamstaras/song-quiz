be9a393f085818fe7697901489c55ebec3f45c7b
Liam Staras <liam.staras@gmail.com>
2020-01-07 19:17:03 +0000

    Add CSV read routine

--- game.py
+++ game.py
@@ -1,2 +1,9 @@
 ### Music quiz for OCR GCSE Computer Science (9-1) ###
 ## Copyright 2020 Liam Staras
+
+# import required libraries
+import csv
+
+# read song database from csv
+with open('songs.csv') as csvfile:
+    songs = list(csv.reader(csvfile))
