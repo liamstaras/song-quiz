0394670aa0f03d77d4d9c0cc61afa0546a5b4438
Liam Staras <liam.staras@gmail.com>
2020-01-07 19:35:20 +0000

    Add comments to game routine

--- game.py
+++ game.py
@@ -13,19 +13,19 @@ with open('songs.csv') as csvfile:
     songs = list(csv.reader(csvfile))[1:] # actually read in the songs, skip header row
 
 # game routine
-attempts = 0
-while not(attempts >= ATTEMPTS):
-    attempts = 0
-    song = random.choice(songs)
+attempts = 0 # set counter to enter while loop [in another programming language, a REPEAT...UNTIL or equivalent loop would be most appropriate as this would not have to be defined twice]
+while not(attempts >= ATTEMPTS): # halt executuion if attemp cap exceeded (2nd test)
+    attempts = 0 # reset counter for each new song
+    song = random.choice(songs) # select a random song
     print('Guess the song!')
-    while not(attempts >= ATTEMPTS):
+    while not(attempts >= ATTEMPTS): # halt executuion if attemp cap exceeded (1st test)
         if attempts > 0:
             print('try again')
         print('Artist: '+song[1])
-        print('Song: '+song[0])
-        guess = input()
+        print('Song: '+song[0]) # NEXT: add routine to obliterate part of the name
+        guess = input() # NEXT: add case redundancy; FUTURE: add fancy overwriting input?
         if guess == song[0]:
             print('Well done!')
-            break
+            break # drop out of loop on correct attempt and pick new song
         else:
-            attempts += 1
+            attempts += 1 # ONLY increase counter if attempt failed
