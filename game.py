c5c1bcc7fcfcfccafdece3376b5b4c134e958b2c
Liam Staras <liam.staras@gmail.com>
2020-01-08 06:59:50 +0000

    Comment authentication subsystem

--- game.py
+++ game.py
@@ -24,36 +24,38 @@ with open('songs.csv') as csvfile:
     songs = list(csv.reader(csvfile))[1:] # actually read in the songs, skip header row
 
 ## main program
-# authentication subsystem
+## authentication subsystem
 print('Please provide credentials.')
+# read all users from csv
 with open('users.csv') as csvfile:
-    users = list(csv.reader(csvfile))[1:]
-username = input('Username: ')
-foundUser = False
+    users = list(csv.reader(csvfile))[1:] # read all users into a list of lists [Python equiv. of array]
+username = input('Username: ') # prompt for the username
+foundUser = False # we are looping through all users in a linear search; we need to know if we found anything afterwards
 for user in users:
-    if user[0] == username:
-        foundUser = True
-        break
+    if user[0] == username: # comparing each row from the csv
+        foundUser = True # make note if the user is found
+        break # we don't need to check any more users if one is found
 if foundUser: # in this case, the user already exists
-    while True:
-        password = input('Password: ')
-        salt = bytes.fromhex(user[2])
-        hash = hashlib.pbkdf2_hmac('sha256',password.encode('utf-8'),salt,100000)
-        if hash.hex() == user[1]:
-            break
+    while True: # we'll just keep going indefinitely here - who would brute-force a command line song quiz anyway? [and the spec. does not tell us to have resilience to this sort of attack]
+        password = input('Password: ') # ask the user for their password
+        salt = bytes.fromhex(user[2]) # get the salt from the database
+        hash = hashlib.pbkdf2_hmac('sha256',password.encode('utf-8'),salt,100000) # generate a hash from the password and salt using 100,000 iterations of SHA256
+        if hash.hex() == user[1]: # if the generated hash is the same as the stored hash, the password was correct
+            break # if the password was correct, we're done and the user is authenticated
         else:
             print('Incorrect password.')
 else: # otherwise, we create a new user
     print('User not found; creating new user.')
-    password = input('Password: ')
-    salt = os.urandom(32)
-    hash = hashlib.pbkdf2_hmac('sha256',password.encode('utf-8'),salt,100000)
-    with open('users.csv','a') as csvfile:
-        csv.writer(csvfile).writerow([username,hash.hex(),salt.hex(),0])
-del(password)
+    password = input('Password: ') # ask the user for a password
+    salt = os.urandom(32) # create a "random" salt
+    hash = hashlib.pbkdf2_hmac('sha256',password.encode('utf-8'),salt,100000) # generate a hash from the password and salt using 100,000 iterations of SHA256
+    with open('users.csv','a') as csvfile: # open the user database in append mode
+        csv.writer(csvfile).writerow([username,hash.hex(),salt.hex(),0]) # write one row to the end of the csv with the username, the hex of the hash and salt, and a best score of 0
+del(password) # delete the password variable - get it out of RAM!
 
+# at this point, we have the variable 'username' as the username of the authenticated user and we can begin the game
 
-# game routine
+## game routine
 attempts = 0 # set counter to enter while loop [in another programming language, a REPEAT...UNTIL or equivalent loop would be most appropriate as this would not have to be defined twice]
 while not(attempts >= ATTEMPTS): # halt executuion if attemp cap exceeded (2nd test)
     attempts = 0 # reset counter for each new song
